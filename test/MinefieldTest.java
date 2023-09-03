3e7417d9cd3c351548c0ada5ca752aafd5d44dc9
tomsr <tom.skancke.rothe@gmail.com>
2023-09-03 22:01:53 +0200

    refactoring

--- test/MinefieldTest.java
+++ test/MinefieldTest.java
@@ -3,14 +3,16 @@ package no.kristiania;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MinefieldTest {
+class MinefieldTest {
 
     @Test
     void shouldEmptyField(){
         String[] expected = { "0" };
-        assertArrayEquals(expected, new Minesweeper(new String[]{ "." }).getHints());
+        assertArrayEquals(
+                expected,
+                new Minesweeper(new String[]{ "." }).getHints()
+        );
     }
 
     @Test
@@ -45,5 +47,22 @@ public class MinefieldTest {
         String[] field = { ".", ".", "*", ".", "." };
         String[] expected = { "0", "1", "*", "1", "0" };
         assertArrayEquals(expected, new Minesweeper(field).getHints());
+    }
+
+    @Test
+    void shouldShowHints() {
+        String[] field = {
+                ".**..",
+                "..*..",
+                ".**..",
+                "...**",
+        };
+        String[] hint = {
+                "1**20",
+                "25*30",
+                "1**32",
+                "123**",
+        };
+
     }
 }
